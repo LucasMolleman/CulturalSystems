ed797468ca75e597be7b2e2431ff49aefd90e1e5
Hannah Armstrong <hannaharmstrong@re-byods-146-50-213-140.wireless.uva.nl>
2024-06-03 14:46:07 +0200

    ggplot graphing

--- graphing.R
+++ graphing.R
@@ -1,6 +1,9 @@
 ## Graphing
 ## Hannah Armstrong
 
+library(ggplot2)
+library(ggpubr)
+
 ## Load data
 
 strategysuccess1 <- read.csv("StrategySuccess")
@@ -20,7 +23,7 @@ bf1 <- strategysuccess1[strategysuccess1$Branching == 1,]
 boxplot(TotalPayoff ~ SLS, data = bf1, main = "Total Payoff per SLS (bf = 1)", ylab = "Total Payoff")
 legend('topright', c('0 = Random', '1 = Payoff', '2 = Similarity', '3 = Age', '4 = Conformity'))
 
-ggplot(data = bf1, aes(x = as.factor(SLS), y = TotalPayoff)) +
+fig1_sig <- ggplot(data = bf1, aes(x = as.factor(SLS), y = TotalPayoff)) +
   theme_light() +
   geom_boxplot() +
   ggtitle("Total Payoff per SLS (bf = 1)") +
@@ -34,6 +37,13 @@ ggplot(data = bf1, aes(x = as.factor(SLS), y = TotalPayoff)) +
               step_increase = 0.1,
               test = "t.test")
 
+fig1 <- ggplot(data = bf1, aes(x = as.factor(SLS), y = TotalPayoff)) +
+  theme_light() +
+  geom_boxplot() +
+  ggtitle("Total Payoff per SLS (bf = 1)") +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold")) +
+  xlab("Social Learning Strategy") +
+  ylab("Total Payoff")
 
 modelbf1 <- aov(TotalPayoff ~ as.factor(SLS), data = bf1)
 summary(modelbf1)
@@ -46,7 +56,7 @@ bf2 <- strategysuccess2[strategysuccess2$Branching == 2,]
 boxplot(TotalPayoff ~ SLS, data = bf2, main = "Total Payoff per SLS (bf = 2)", ylab = "Total Payoff")
 legend('topright', c('0 = Random', '1 = Payoff', '2 = Similarity', '3 = Age', '4 = Conformity'))
 
-ggplot(data = bf2, aes(x = as.factor(SLS), y = TotalPayoff)) +
+fig2_sig <- ggplot(data = bf2, aes(x = as.factor(SLS), y = TotalPayoff)) +
   theme_light() +
   geom_boxplot() +
   ggtitle("Total Payoff per SLS (bf = 2)") +
@@ -61,6 +71,14 @@ ggplot(data = bf2, aes(x = as.factor(SLS), y = TotalPayoff)) +
               test = "t.test")
 
 
+fig2 <- ggplot(data = bf2, aes(x = as.factor(SLS), y = TotalPayoff)) +
+  theme_light() +
+  geom_boxplot() +
+  ggtitle("Total Payoff per SLS (bf = 2)") +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold")) +
+  xlab("Social Learning Strategy") +
+  ylab("Total Payoff")
+
 modelbf2 <- aov(TotalPayoff ~ as.factor(SLS), data = bf2)
 summary(modelbf2)
 TukeyHSD(modelbf2)
@@ -72,7 +90,7 @@ bf4 <- strategysuccess1[strategysuccess1$Branching == 4,]
 boxplot(TotalPayoff ~ SLS, data = bf4, main = "Total Payoff per SLS (bf = 4)", ylab = "Total Payoff")
 legend('topright', c('0 = Random', '1 = Payoff', '2 = Similarity', '3 = Age', '4 = Conformity'))
 
-ggplot(data = bf4, aes(x = as.factor(SLS), y = TotalPayoff)) +
+fig3_sig <- ggplot(data = bf4, aes(x = as.factor(SLS), y = TotalPayoff)) +
   theme_light() +
   geom_boxplot() +
   ggtitle("Total Payoff per SLS (bf = 4)") +
@@ -86,6 +104,13 @@ ggplot(data = bf4, aes(x = as.factor(SLS), y = TotalPayoff)) +
               step_increase = 0.1,
               test = "t.test")
 
+fig3 <- ggplot(data = bf4, aes(x = as.factor(SLS), y = TotalPayoff)) +
+  theme_light() +
+  geom_boxplot() +
+  ggtitle("Total Payoff per SLS (bf = 4)") +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold")) +
+  xlab("Social Learning Strategy") +
+  ylab("Total Payoff")
 
 modelbf4 <- aov(TotalPayoff ~ as.factor(SLS), data = bf4)
 summary(modelbf4)
@@ -98,7 +123,7 @@ bf8 <- strategysuccess2[strategysuccess2$Branching == 8,]
 boxplot(TotalPayoff ~ SLS, data = bf8, main = "Total Payoff per SLS (bf = 8)", ylab = "Total Payoff")
 legend('topright', c('0 = Random', '1 = Payoff', '2 = Similarity', '3 = Age', '4 = Conformity'))
 
-ggplot(data = bf8, aes(x = as.factor(SLS), y = TotalPayoff)) +
+fig4_sig <- ggplot(data = bf8, aes(x = as.factor(SLS), y = TotalPayoff)) +
   theme_light() +
   geom_boxplot() +
   ggtitle("Total Payoff per SLS (bf = 8)") +
@@ -112,6 +137,13 @@ ggplot(data = bf8, aes(x = as.factor(SLS), y = TotalPayoff)) +
               step_increase = 0.1,
               test = "t.test")
 
+fig4 <- ggplot(data = bf8, aes(x = as.factor(SLS), y = TotalPayoff)) +
+  theme_light() +
+  geom_boxplot() +
+  ggtitle("Total Payoff per SLS (bf = 8)") +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold")) +
+  xlab("Social Learning Strategy") +
+  ylab("Total Payoff")
 
 modelbf8 <- aov(TotalPayoff ~ as.factor(SLS), data = bf8)
 summary(modelbf8)
@@ -124,7 +156,7 @@ bf16 <- strategysuccess2[strategysuccess2$Branching == 16,]
 boxplot(TotalPayoff ~ SLS, data = bf16, main = "Total Payoff per SLS (bf = 16)", ylab = "Total Payoff")
 legend('topright', c('0 = Random', '1 = Payoff', '2 = Similarity', '3 = Age', '4 = Conformity'))
 
-ggplot(data = bf16, aes(x = as.factor(SLS), y = TotalPayoff)) +
+fig5_sig <- ggplot(data = bf16, aes(x = as.factor(SLS), y = TotalPayoff)) +
   theme_light() +
   geom_boxplot() +
   ggtitle("Total Payoff per SLS (bf = 16)") +
@@ -138,6 +170,13 @@ ggplot(data = bf16, aes(x = as.factor(SLS), y = TotalPayoff)) +
               step_increase = 0.1,
               test = "t.test")
 
+fig5 <- ggplot(data = bf16, aes(x = as.factor(SLS), y = TotalPayoff)) +
+  theme_light() +
+  geom_boxplot() +
+  ggtitle("Total Payoff per SLS (bf = 16)") +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold")) +
+  xlab("Social Learning Strategy") +
+  ylab("Total Payoff")
 
 modelbf16 <- aov(TotalPayoff ~ as.factor(SLS), data = bf16)
 summary(modelbf16)
@@ -150,7 +189,7 @@ bf32 <- strategysuccess1[strategysuccess1$Branching == 32,]
 boxplot(TotalPayoff ~ SLS, data = bf32, main = "Total Payoff per SLS (bf = 32)", ylab = "Total Payoff")
 legend('topright', c('0 = Random', '1 = Payoff', '2 = Similarity', '3 = Age', '4 = Conformity'))
 
-ggplot(data = bf32, aes(x = as.factor(SLS), y = TotalPayoff)) +
+fig6_sig <- ggplot(data = bf32, aes(x = as.factor(SLS), y = TotalPayoff)) +
   theme_light() +
   geom_boxplot() +
   ggtitle("Total Payoff per SLS (bf = 32)") +
@@ -164,6 +203,14 @@ ggplot(data = bf32, aes(x = as.factor(SLS), y = TotalPayoff)) +
               step_increase = 0.1,
               test = "t.test")
 
+fig6 <- ggplot(data = bf32, aes(x = as.factor(SLS), y = TotalPayoff)) +
+  theme_light() +
+  geom_boxplot() +
+  ggtitle("Total Payoff per SLS (bf = 32)") +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold")) +
+  xlab("Social Learning Strategy") +
+  ylab("Total Payoff")
+
 modelbf32 <- aov(TotalPayoff ~ as.factor(SLS), data = bf32)
 summary(modelbf32)
 TukeyHSD(modelbf32)
@@ -175,7 +222,7 @@ bf64 <- strategysuccess2[strategysuccess2$Branching == 64,]
 boxplot(TotalPayoff ~ SLS, data = bf64, main = "Total Payoff per SLS (bf = 64)", ylab = "Total Payoff")
 legend('topright', c('0 = Random', '1 = Payoff', '2 = Similarity', '3 = Age', '4 = Conformity'))
 
-ggplot(data = bf64, aes(x = as.factor(SLS), y = TotalPayoff)) +
+fig7_sig <- ggplot(data = bf64, aes(x = as.factor(SLS), y = TotalPayoff)) +
   theme_light() +
   geom_boxplot() +
   ggtitle("Total Payoff per SLS (bf = 64)") +
@@ -188,6 +235,14 @@ ggplot(data = bf64, aes(x = as.factor(SLS), y = TotalPayoff)) +
               step_increase = 0.1,
               test = "t.test")
 
+fig7 <- ggplot(data = bf64, aes(x = as.factor(SLS), y = TotalPayoff)) +
+  theme_light() +
+  geom_boxplot() +
+  ggtitle("Total Payoff per SLS (bf = 64)") +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold")) +
+  xlab("Social Learning Strategy") +
+  ylab("Total Payoff")
+
 modelbf64 <- aov(TotalPayoff ~ as.factor(SLS), data = bf64)
 summary(modelbf64)
 TukeyHSD(modelbf64)
@@ -199,7 +254,7 @@ bf128 <- strategysuccess1[strategysuccess1$Branching == 128,]
 boxplot(TotalPayoff ~ SLS, data = bf128, main = "Total Payoff per SLS (bf = 128)", ylab = "Total Payoff")
 legend('topright', c('0 = Random', '1 = Payoff', '2 = Similarity', '3 = Age', '4 = Conformity'))
 
-ggplot(data = bf128, aes(x = as.factor(SLS), y = TotalPayoff)) +
+fig8_sig <- ggplot(data = bf128, aes(x = as.factor(SLS), y = TotalPayoff)) +
   theme_light() +
   geom_boxplot() +
   ggtitle("Total Payoff per SLS (bf = 128)") +
@@ -211,6 +266,17 @@ ggplot(data = bf128, aes(x = as.factor(SLS), y = TotalPayoff)) +
               step_increase = 0.1,
               test = "t.test")
 
+fig8 <- ggplot(data = bf128, aes(x = as.factor(SLS), y = TotalPayoff)) +
+  theme_light() +
+  geom_boxplot() +
+  ggtitle("Total Payoff per SLS (bf = 128)") +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold")) +
+  xlab("Social Learning Strategy") +
+  ylab("Total Payoff")
+
 modelbf128 <- aov(TotalPayoff ~ as.factor(SLS), data = bf128)
 summary(modelbf128)
 TukeyHSD(modelbf128)
+
+combinedfigure1 <- ggarrange(fig1, fig2, fig3, fig4)
+combinedfigure2 <- ggarrange(fig5, fig6, fig7, fig8)
